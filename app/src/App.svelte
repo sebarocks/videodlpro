9b891ebff9bc73bb7c69e6e598100e08934a9419
Sebastian Orrego <sebastian_orrego_a@outlook.com>
2022-08-24 23:30:00 -0400

    change main component style

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,42 +1,40 @@
 <script>
-import ProgressBar from './lib/DownloadBar.svelte';
-import UrlField from './lib/UrlField.svelte';
-import VideoCard from './lib/VideoCard.svelte';
+  import ProgressBar from "./lib/DownloadBar.svelte";
+  import UrlField from "./lib/UrlField.svelte";
+  import VideoCard from "./lib/VideoCard.svelte";
 
-let videos = [];
+  let videos = [];
 
-function addUrl(ev){
-  console.log(`Se agrego ${ev.detail}`);
-  videos = [...videos, ev.detail];
-}
-
+  function addUrl(ev) {
+    console.log(`Se agrego ${ev.detail}`);
+    videos = [...videos, ev.detail];
+  }
 </script>
 
 <main>
   <div>
-    <img src="/logo.svg" class="logo" alt="YT Logo" />
+    <img src="/logo.png" class="logo" alt="YT Logo" />
   </div>
   <h1>Video-DL Pro</h1>
 
   <div class="urlfield">
-    <UrlField on:addedUrl={addUrl}/>    
+    <UrlField on:addedUrl={addUrl} />
   </div>
   {#each videos as vidUrl}
-    <VideoCard url={vidUrl}/>
+    <VideoCard url={vidUrl} />
   {/each}
-
 </main>
 
 <style>
   .logo {
-    height: 3em;
+    height: 10em;
     padding: 0.5em;
     will-change: filter;
   }
   .logo:hover {
-    filter: drop-shadow(0 0 2em #ea1244aa);
+    filter: drop-shadow(0 0 2em #12ea85aa);
   }
-  .urlfield{
+  .urlfield {
     width: 30rem;
   }
 </style>
