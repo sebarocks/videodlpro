05795e23c5ce175b726b21a4626058fb48a25da0
Sebastian Orrego <sebastian_orrego_a@outlook.com>
2022-09-02 12:53:54 -0400

    static adapter

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,9 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		  }),
+		  
+	},
+	path: {
+		assets: 'assets',
 	}
 };
 
