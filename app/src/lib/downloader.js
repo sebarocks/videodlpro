16a1ff495a5f5b350d8f753b4c52ecc158e924d0
Sebastian Orrego <sebastian_orrego_a@outlook.com>
2022-09-04 01:35:23 -0300

    fix hostname socket

--- app/src/lib/downloader.js
+++ app/src/lib/downloader.js
@@ -1,8 +1,8 @@
 import io from "socket.io-client";
 import { env } from '$env/dynamic/public';
 
-console.log(`ws://${env.PUBLIC_API_URL}`);
-const socket = io(`ws://${env.PUBLIC_HOSTNAME}`, { path: "/api/ws/" });
+console.log(env.PUBLIC_HOSTNAME);
+const socket = io(`${env.PUBLIC_HOSTNAME}`, { path: "/api/ws/" });
 
 export default socket;
 
